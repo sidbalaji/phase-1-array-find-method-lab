2e0f7a12fe5c1e3f7775fbac39628dbf240fe45f
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-11 15:48:47 -0500

    completed with tests passing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function winTest(element){
+    return element.result == 'W'
+    }
+
+function superbowlWin(arr){
+        let index = arr.find(winTest)
+        //console.log(index)
+        if(typeof index ==='undefined'){
+            return index
+        }
+        else{
+        return index.year
+        }
+    }
